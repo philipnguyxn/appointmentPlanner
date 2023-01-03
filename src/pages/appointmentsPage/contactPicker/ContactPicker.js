4d5480f4e83dc3f7de99da4d9af7a4864745321c
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-03 11:13:06 +0700

    add select elemnt in ContactPicker and  test it

--- src/pages/appointmentsPage/contactPicker/ContactPicker.js
+++ src/pages/appointmentsPage/contactPicker/ContactPicker.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
-  return;
-};
+export const ContactPicker = ({ contacts, setContact }) => (
+  <div className="select-wrapper">
+    <select name="contact" onChange={(e) => setContact(e.target.value)}>
+      <option id="header-option">Select a contact</option>
+      {contacts?.map((contact) => (
+        contact.name && (
+          <option value={contact.name}>{contact.name}</option>
+        )
+      ))}
+    </select>
+  </div>
+)
