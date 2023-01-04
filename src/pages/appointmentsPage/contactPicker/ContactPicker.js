e5502fc101276175649574f55bb007ea77373379
Phil_nguyxn <khoinm2003@gmail.com>
2023-01-04 10:06:51 +0700

    add handleChange()

--- src/pages/appointmentsPage/contactPicker/ContactPicker.js
+++ src/pages/appointmentsPage/contactPicker/ContactPicker.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-export const ContactPicker = ({ contacts, setContact }) => (
-  <div className="select-wrapper">
-    <select name="contact" onChange={(e) => setContact(e.target.value)}>
-      <option id="header-option">Select a contact</option>
-      {contacts?.map((contact) => (
-        contact.name && (
-          <option value={contact.name}>{contact.name}</option>
-        )
-      ))}
-    </select>
-  </div>
-)
+export const ContactPicker = ({ contacts, setContact }) => {
+  const handleChange = (e) => {
+    const contact = JSON.parse(e.target.value);
+
+    setContact({
+      name: contact.name,
+      phoneNumber: contact.phoneNumber,
+      email: contact.email
+    });
+  }
+
+  return (
+    <div className="select-wrapper">
+      <select name="contact" onChange={handleChange}>
+        <option id="header-option">Select a contact</option>
+        {contacts?.map((contact) => (
+          contact.name && (
+            <option value={JSON.stringify(contact)}>{contact.name}</option>
+          )
+        ))}
+      </select>
+    </div>
+  )
+}
